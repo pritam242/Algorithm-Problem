e548f4fe0e0bf4e6817124d516d25892575b929b
Pritam Das <59303654+pritam242@users.noreply.github.com>
2020-01-17 12:14:05 +0530

    Add files via upload

--- Mini-Max/Min-Max.py
+++ Mini-Max/Min-Max.py
@@ -1,1 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 12:13:05 2020
 
+@author: Pritam
+"""
+
+def miniMaxSum(arr):
+    result=sum(arr)
+
+    print(result-(max(arr)), result-(min(arr)))
+
+if __name__ == '__main__':
+    arr = list(map(int, input().strip().split()))
+
+    miniMaxSum(arr)
