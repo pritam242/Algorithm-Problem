39fe132902bca7ebe55db60c0e429052379da25c
Pritam Das <59303654+pritam242@users.noreply.github.com>
2020-01-17 11:32:37 +0530

    Add files via upload

--- Plus-Minus/Plus-Minus.py
+++ Plus-Minus/Plus-Minus.py
@@ -1,1 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 11:31:35 2020
 
+@author: Pritam
+"""
+
+def plusMinus(arr):
+    print (len([x for x in arr if x > 0])/n)
+    print (len([x for x in arr if x < 0])/n)
+    print (len([x for x in arr if x == 0])/n)
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
