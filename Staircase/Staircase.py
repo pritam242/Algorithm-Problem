6ca9df7ea4ff59528c288e17a4490cd625717c85
Pritam Das <59303654+pritam242@users.noreply.github.com>
2020-01-17 12:07:32 +0530

    Add files via upload

--- Staircase/Staircase.py
+++ Staircase/Staircase.py
@@ -1,1 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 12:06:13 2020
 
+@author: Pritam
+"""
+
+def staircase(n):
+    for i in range(1, n + 1):
+        print(str('#'*i).rjust(n))
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
