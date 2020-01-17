4ed1a5a944346be9fccdddf0d3bf167f4fb90e96
Pritam Das <59303654+pritam242@users.noreply.github.com>
2020-01-17 12:27:40 +0530

    Add files via upload

--- Birthday Cake Candles/Birthday Cake Candles.py
+++ Birthday Cake Candles/Birthday Cake Candles.py
@@ -1,1 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 12:26:23 2020
 
+@author: Pritam
+"""
+
+def birthdayCakeCandles(ar):
+    count=0
+    big = max(ar)
+    for i in range(len(ar)):
+        if(ar[i]==big):
+            count+=1
+    return count         
+
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
